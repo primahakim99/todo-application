14856f068f4455cce8dec31edddfb6689a6165a7
primahakim99 <fahreza.prima@gmail.com>
2022-06-08 13:31:30 +0700

    Practicum2

---
+++ database/migrations/2022_06_08_020420_create_todolist_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTodolistTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('todolist', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->refrences('id')->on('users');
+            $table->string('todo');
+            $table->string('done');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('todolist');
+    }
+}
